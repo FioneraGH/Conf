10f8dcff7af612e5108b3fb51b20169c037d557b
fionera <fionera@linux.site>
2015-07-13 14:19:25 +0800

    update javapath

--- java.sh
+++ java.sh
@@ -1,3 +1,3 @@
-export JAVA_HOME=/home/fionera/LinuxIDE/jdk1.8.0_25
+export JAVA_HOME=/home/fionera/LinuxIDE/jdk
 export CLASS_PATH=.:$JAVA_HOME/lib/tool.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/jre/lib/rt.jar
 export PATH=$PATH:$JAVA_HOME/bin 
