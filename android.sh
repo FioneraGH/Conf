68f2fbf7801ddd9baaeb5d43beb4894009fc4500
FioneraGH <fionera@outlook.com>
2015-12-15 18:58:15 +0800

    add an android.sh

---
+++ android.sh
@@ -0,0 +1,3 @@
+export ANDROID_HOME=/home/fionera/LinuxIDE/android-sdk-linux
+export ANDROID_NDK=/home/fionera/LinuxIDE/android-sdk-linux/android-ndk-r10e
+export PATH=$PATH:$ANDROID_NDK:$ANDROID_HOME/platform-tools
