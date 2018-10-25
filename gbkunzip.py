1ceaa5c4754b107743335c0ac98164cbf724abef
FioneraGH <fionera@outlook.com>
2018-10-25 10:11:15 +0800

    Convert gbkunzip.py from version 2 to 3

--- gbkunzip.py
+++ gbkunzip.py
@@ -1,22 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
 import sys
 import zipfile
 
-print "Processing File " + sys.argv[1]
 
-file=zipfile.ZipFile(sys.argv[1],"r");
-for name in file.namelist():
-    utf8name=name.decode('gbk')
-    print "Extracting " + utf8name
-    pathname = os.path.dirname(utf8name)
-    if not os.path.exists(pathname) and pathname!= "":
-        os.makedirs(pathname)
-    data = file.read(name)
-    if not os.path.exists(utf8name):
-        fo = open(utf8name, "w")
-        fo.write(data)
-        fo.close
-file.close()
+def main():
+    print("Processing File ", sys.argv[1])
+
+    with zipfile.ZipFile(sys.argv[1], "r") as file:
+        for name in file.namelist():
+            utf8name = name.decode('gbk')
+            print("Extracting ", utf8name)
+            pathname = os.path.dirname(utf8name)
+            if not os.path.exists(pathname) and pathname != "":
+                os.makedirs(pathname)
+            data = file.read(name)
+            if not os.path.exists(utf8name):
+                fo = open(utf8name, "w")
+                fo.write(data)
+                fo.close
+
+
+if __name__ == '__main__':
+    main()
